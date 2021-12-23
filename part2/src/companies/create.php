703cc39734827a85f9a9d65b468fb43e3d83b26e
mac <tmakiryo8199@yahoo.co.jp>
2021-12-24 05:49:39 +0900

    会社情報PHPファイルとHTMLファイル分割

--- part2/src/companies/create.php
+++ part2/src/companies/create.php
@@ -36,7 +36,7 @@ function validate($company)
     }
 
     // 設立日
-    $dates = explode('-', $company['date']);
+    $dates = explode('-', $company['establishment_date']);
 
     if (!strlen($company['establishment_date'])) {
         $errors['establishment_date'] = '設立日を入力してください';
@@ -72,43 +72,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header("Location: index.php");
     }
 }
-?>
 
-<!DOCTYPE html>
-<html lang="jp">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>会社情報の登録</title>
-</head>
-
-<body>
-    <h1>会社情報の登録</h1>
-    <form action="create.php" method='post'>
-        <?php if (count($errors)) : ?>
-            <ul>
-                <?php foreach ($errors as $error) : ?>
-                    <li><?php echo $error; ?></li>
-                <?php endforeach; ?>
-            </ul>
-        <?php endif; ?>
-        <div>
-            <label for="name">会社名</label>
-            <input type="text" name="name" id="name">
-        </div>
-        <div>
-            <label for="establishment_date">設立日</label>
-            <input type="date" name="establishment_date" id="establishment_date">
-        </div>
-        <div>
-            <label for="founder">代表者</label>
-            <input type="text" name="founder" id="founder">
-        </div>
-
-        <button type="submit">登録する</button>
-    </form>
-</body>
-
-</html>
+include 'views/new.php';
